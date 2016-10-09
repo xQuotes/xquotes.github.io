80d8a372b0fb3d17269fe434f83a5e3e74bd196e
fooying <fooying@qq.com>
2016-10-09 16:05:22 +0800

    drag pic

--- dazangjing/src/route/jiaokan/pic/picLines.js
+++ dazangjing/src/route/jiaokan/pic/picLines.js
@@ -1,96 +1,114 @@
-import { Button } from 'antd'
+import {
+  Button, Checkbox, Icon
+} from 'antd'
+var WidthProvider = require('react-grid-layout').WidthProvider
+var ReactGridLayout = require('react-grid-layout')
+ReactGridLayout = WidthProvider(ReactGridLayout)
 
 import PicLine from './picLine'
+
+
+const layouts = [
+  {i: '0', w: 20},
+  {i: '1', w: 30},
+  {i: '2', w: 20}
+]
 
 export default class PicLines extends React.Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      layout: [{
+        i: '0',
+        w: '20',
+        h: 1,
+        maxH: 1,
+        y: 0,
+        x: 0
+      }]
+    }
+  }
+  // componentWillReceiveProps
+  componentWillMount() {
+    let layout = []
+
+    layout = _.map(layouts, (v, k) => {
+      return {
+        i: v.i,
+        w: v.w,
+        h: 1,
+        maxH: 1,
+        y: 0,
+        x: _.sumBy(_.dropRight(layouts, layouts.length-k), 'w')
+      }
+    })
+
+    this.setState({
+      layout: layout
+    })
+
+  }
+  onResize(layout,
+                oldItem,
+                newItem,
+                placeholder,
+                e,
+                element) {
+    let layoutData = layout,
+    layoutDatas = []
+    layoutData[newItem.i] = newItem
+    layoutDatas = _.map(layoutData, (v, k) => {
+      return {
+        i: v.i,
+        w: v.w,
+        h: 1,
+        maxH: 1,
+        y: 0,
+        x: _.sumBy(_.dropRight(layoutData, layouts.length-k), 'w')
+      }
+    })
+    this.setState({
+      layout: layoutDatas
+    })
+
   }
   render() {
-    const lines = [{
-      status: 0, // 四种状态
-      width: 50, // 总长800
-    },{
-      status: 0, // 四种状态
-      width: 40, // 总长800
-    },{
-      status: 0, // 四种状态
-      width: 28, // 总长800
-    },{
-      status: 0, // 四种状态
-      width: 30, // 总长800
-    },{
-      status: 0, // 四种状态
-      width: 35, // 总长800
-    },{
-      status: 0, // 四种状态
-      width: 35, // 总长800
-    },{
-      status: 0, // 四种状态
-      width: 32, // 总长800
-    },{
-      status: 0, // 四种状态
-      width: 30, // 总长800
-    },{
-      status: 0, // 四种状态
-      width: 32, // 总长800
-    },{
-      status: 0, // 四种状态
-      width: 31, // 总长800
-    },{
-      status: 0, // 四种状态
-      width: 31, // 总长800
-    },{
-      status: 0, // 四种状态
-      width: 31, // 总长800
-    },{
-      status: 0, // 四种状态
-      width: 31, // 总长800
-    },{
-      status: 0, // 四种状态
-      width: 34, // 总长800
-    },{
-      status: 0, // 四种状态
-      width: 35, // 总长800
-    },{
-      status: 0, // 四种状态
-      width: 31, // 总长800
-    },{
-      status: 0, // 四种状态
-      width: 31, // 总长800
-    },{
-      status: 0, // 四种状态
-      width: 31, // 总长800
-    },{
-      status: 0, // 四种状态
-      width: 31, // 总长800
-    },{
-      status: 0, // 四种状态
-      width: 31, // 总长800
-    },{
-      status: 0, // 四种状态
-      width: 31, // 总长800
-    },{
-      status: 0, // 四种状态
-      width: 31, // 总长800
-    },{
-      status: 0, // 四种状态
-      width: 30, // 总长800
-    },{
-      status: 0, // 四种状态
-      width: 16, // 总长800
-    },{
-      status: 0, // 四种状态
-      width: 31, // 总长800
-    }]
+    const that = this
+    const styles = {
+      width: 800,
+      cols: 800,
+      className: 'layout',
+      rowHeight: 550,
+      height: 550,
+      margin: [0, 0],
+      padding: [0, 0],
+      isDraggable: false,
+      onResize: that.onResize.bind(that)
+    }
+    const { layout } = this.state
+    
     return(
       <div className="pic-lines">
-        {
-          _.map(lines, (value, key)=> {
-            return <PicLine value={{...value, key}} key={key} />
-          })
-        }
+        <ReactGridLayout
+          {...styles}
+          layout={layout}>
+          {_.map(layout, (value, key)=> {
+            return <div key={value.i} className="pic-line">
+              <div className="pic-line-close">
+                <Icon type="close" />
+              </div>
+              <div className="pic-line-checkbox">
+                <Checkbox></Checkbox>
+              </div>
+              <div className="pic-line-num">
+                {key}
+              </div>
+            </div>
+          })}
+        </ReactGridLayout>
       </div>
+
       )
   }
 }
