81c6b5886c518a7f33bab521ff78e555ac18b151
fooying <fooying@qq.com>
2016-10-09 16:12:52 +0800

    deploy

--- dist/4.4.198898d7eb5ed2db3a9c.js
+++
@@ -1,2 +0,0 @@
-/*! This file is created by fooying@qq.com */
-webpackJsonp([4],{261:function(t,o,e){"use strict";t.exports={path:"jiao",component:e(88)["default"]}}});
---
+++ dist/4.4.f0359e73d75def52e01f.js
@@ -0,0 +1,2 @@
+/*! This file is created by fooying@qq.com */
+webpackJsonp([4],{269:function(t,o,e){"use strict";t.exports={path:"jiao",component:e(91)["default"]}}});
