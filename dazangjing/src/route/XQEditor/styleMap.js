426a6a9544cb5e96bab77d48d0dab761bb644995
fooying <fooying@qq.com>
2016-10-12 17:48:33 +0800

    richtext

---
+++ dazangjing/src/route/XQEditor/styleMap.js
@@ -0,0 +1,21 @@
+export const bgColorStyleMap = {
+  bgPurple: {
+    backgroundColor: 'purple'
+  },
+  bgBlue: {
+    backgroundColor: 'blue'
+  },
+  bgOrange: {
+    backgroundColor: 'orange'
+  },
+  bgYellow: {
+    backgroundColor: 'yellow'
+  }
+}
+
+export const BGCOLORS = [
+  {type: 'bg', label: '广义异体字', style: 'bgPurple'},
+  {type: 'bg', label: '广义正字', style: 'bgBlue'},
+  {type: 'bg', label: '易错字词', style: 'bgOrange'},
+  {type: 'bg', label: '难字', style: 'bgYellow'}
+]
